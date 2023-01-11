972317bd2796d1b14b5a15c12882b12cfc4299c4
davidsstefano <davidsstefano@gmail.com>
2023-01-11 16:57:30 -0300

    master

--- api/api_cadastrousuario.php
+++ api/api_cadastrousuario.php
@@ -16,13 +16,13 @@ if (isset($_POST['cpf'])) {
      $nascimento = addslashes($_POST['nascimento']);
      $admissao = addslashes($_POST['admissao']);
      $cargo = addslashes($_POST['cargo']);
-     $categoria = addslashes($_POST['categoria']);
      $cep = addslashes($_POST['cep']);
      $estado = addslashes($_POST['estado']);
      $cidade = addslashes($_POST['cidade']);
      $bairro = addslashes($_POST['bairro']);
      $endereco = addslashes($_POST['endereco']);
      $numero = addslashes($_POST['numero']);
+     $categoria = addslashes($_POST['categoria']);
 
      var_dump($_POST);
 
@@ -32,14 +32,14 @@ if (isset($_POST['cpf'])) {
      // $result = $conn->select($sql, array('cpf' => $cpf, 'email' => $email));
 
 
-     $sql = "INSERT INTO  tbl_usuario (nome,sobrenome,email,senha,telefone,cpf,nascimento,admissao,cargo,categoria,cep,estado,cidade,bairro,endereco,numero)
-          VALUES(:nome,:sobrenome,:email,:senha,:telefone,:cpf,:nascimento,:admissao,:cargo,:categoria,:cep,:estado,:cidade,:bairro,:endereco,:numero)";
+     $sql = "INSERT INTO  tbl_usuario (nome,sobrenome,email,senha,telefone,cpf,nascimento,admissao,cargo,cep,estado,cidade,bairro,endereco,numero,categoria)
+          VALUES(:nome,:sobrenome,:email,:senha,:telefone,:cpf,:nascimento,:admissao,:cargo,:cep,:estado,:cidade,:bairro,:endereco,:numero,:categoria)";
 
      $novo_id = $conn->insert($sql, array(
           'nome' => $nome, 'sobrenome' => $sobrenome, 'email' => $email,
           'senha' => $senha, 'telefone' => $telefone, 'cpf' => $cpf, 'nascimento' => $nascimento, 'admissao' => $admissao,
-          'cargo' => $cargo, 'categoria' => $categoria, 'cep' => $cep, 'estado' => $estado, 'cidade' => $cidade, 'bairro' => $bairro,
-          'endereco' => $endereco, 'numero' => $numero
+          'cargo' => $cargo, 'cep' => $cep, 'estado' => $estado, 'cidade' => $cidade, 'bairro' => $bairro,
+          'endereco' => $endereco, 'numero' => $numero, 'categoria' => $categoria
      ));
 }
 
--- public/cadastro_funcionario.php
+++ public/cadastro_funcionario.php
@@ -26,39 +26,102 @@ require_once '../class/retorno.php';
 <body>
 
      <!-- <form id="formlogin" method="POST" enctype="multipart/form-data"> -->
-     <form action="../api/api_cadastrousuario.php" method="POST">
+     <form action="../api/api_cadastrousuario.php" method="POST" class="row g-3">
           <h1 class="titulo-login">Cadastro funcionario</h1>
 
 
-          <input type="text" name="nome" placeholder="Nome" maxlength="40" required class="form-input">
-          <input type="text" name="sobrenome" placeholder="Sobrenome" maxlength="40" required class="form-input">
-          <input type="email" name="email" placeholder="E-mail" maxlength="40" required class="form-input">
-          <input type="password" name="senha" placeholder="Senha" maxlength="32" required class="form-input">
-          <input type="tel" name="telefone" placeholder="Telefone" maxlength="40" required class="form-input">
-          <input type="text" name="cpf" placeholder="CPF" maxlength="40" required class="form-input">
-          <input type="date" name="nascimento" placeholder="Data-nascimento" maxlength="40" required class="form-input">
-          <input type="date" name="admissao" placeholder="Data-admissão" maxlength="40" required class="form-input">
-          <input type="text" name="cargo" placeholder="Cargo" maxlength="40" required class="form-input">
-          <select name="cargo" required>
-               <option value="0" selected disabled>Selecione o Cargo:</option>
+          <div class="col-md-5">
+               <label for="inputEmail4" class="form-label">Nome</label>
+               <input type="nome" name="nome" class="form-control" id="inputEmail4" maxlength="40" required>
+          </div>
+          <div class="col-md-5">
+               <label for="inputPassword4" class="form-label">Sobrenome</label>
+               <input type="text" name="sobrenome" class="form-control" id="inputPassword4" maxlength="40" required>
+          </div>
+          <div class="col-md-5">
+               <label for="inputAddress" class="form-label">email</label>
+               <input type="text" name="email" class="form-control" id="inputAddress" placeholder="" maxlength="40" required>
+          </div>
+          <div class="col-md-5">
+               <label for="inputAddress2" class="form-label">Senha</label>
+               <input type="password" name="senha" class="form-control" id="inputAddress2" placeholder="" maxlength="32" required>
+          </div>
+          <div class="col-md-3">
+               <label for="inputAddress2" class="form-label">Contato</label>
+               <input type="tel" name="nome" class="form-control" id="inputAddress2" placeholder="ex: (ddd)999999999">
+          </div>
+          <div class="col-5">
+               <label for="inputAddress" class="form-label">CPF</label>
+               <input type="text" name="cpf" class="form-control" id="inputAddress" placeholder="" maxlength="11" required>
+          </div>
+          <div class="col-md-2">
+               <label for="inputZip" class="form-label">Data-nascimento</label>
+               <input type="date" name="nascimento" class="form-control" id="inputZip" placeholder="" maxlength="12" required>
+          </div>
+          <div class="col-md-2">
+               <label for="inputState" class="form-label">Data-admissão</label>
+               <input type="date" name="admissao" class="form-control" id="inputZip" placeholder="" maxlength="12" required>
+          </div>
 
-               <?php
-               foreach ($db_cargo as $output) {
-                    echo '<option value="' . $output['id_cargo'] . '">' . $output['nome'] . '</option>';
-               }
-               ?>
-          </select>
-          <select name="categoria">
-               <option value="1">Administrador</option>
-               <option value="2">Funcionario</option>
-          </select>
-          <input type="text" name="cep" placeholder="Cep" id="cep" onblur="pesquisacep(this.value);" maxlength="40" required class="form-input">
-          <input type="text" name="estado" placeholder="Estado"  id="uf"  maxlength="40" required class="form-input">
-          <input type="text" name="cidade" placeholder="Cidade" maxlength="40" id="cidade" required class="form-input">
-          <input type="text" name="bairro" placeholder="Bairro" maxlength="40" id="bairro" required class="form-input">
-          <input type="text" name="endereco" placeholder="Endereço" maxlength="40" id="endereco" required class="form-input">
-          <input type="text" name="numero" placeholder="Numero" maxlength="40" required class="form-input">
-          <input type="submit" value="Cadastrar">
+          <div class="col-md-2">
+               <label for="inputState" class="form-label">Cargo</label>
+               <select name="cargo" id="inputState" class="form-select" required>
+                    <option value="0" selected disabled>Selecione o Cargo:</option>
+
+                    <?php
+                    foreach ($db_cargo as $output) {
+                         echo '<option value="' . $output['id_cargo'] . '">' . $output['nome'] . '</option>';
+                    }
+                    ?>
+               </select>
+          </div>
+          <div class="col-md-3">
+               <label for="cep" class="form-label">CEP</label>
+               <input type="text" name="cep" class="form-control" placeholder="CEP" id="cep" onblur="pesquisacep(this.value);" maxlength="40" required class="form-input">
+          </div>
+          <div class="col-md-5">
+               <label for="uf" class="form-label">Estado</label>
+               <input type="text" name="estado" class="form-control" placeholder="Estado" id="uf" maxlength="40" required>
+          </div>
+          <div class="col-md-4">
+               <label for="cidade" class="form-label">Cidade</label>
+               <input type="text" name="cidade" class="form-control" placeholder="Cidade" maxlength="40" id="cidade" required>
+          </div>
+
+          <div class="col-md-5">
+               <label for="bairro" class="form-label">Bairro</label>
+               <input type="text" class="form-control" name="bairro" placeholder="Bairro" maxlength="40" id="bairro" required>
+          </div>
+          <div class="col-md-4">
+               <label for="endereco" class="form-label">Endereço</label>
+               <input type="text" class="form-control" name="endereco" placeholder="Endereço" maxlength="40" id="endereco" required>
+          </div>
+          <div class="col-md-1">
+               <label for="inputZip" class="form-label">Número</label>
+               <input type="text" class="form-control" name="numero" placeholder="Numero" maxlength="5" required id="inputZip">
+          </div>
+          <div>
+
+               <div class="col" id="status">
+                    <div class="form-check form-check-inline">
+                         <input class="form-check-input" type="radio" name="categoria" id="inlineCheckbox1" value="option1">
+                         <label class="form-check-label" for="inlineCheckbox1">Administrador</label>
+                    </div>
+
+                    <div class="form-check form-check-inline">
+                         <input class="form-check-input" type="radio" name="categoria" id="inlineCheckbox2" value="option2">
+                         <label class="form-check-label" for="inlineCheckbox2">Colaborador</label>
+                    </div>
+               </div>
+
+          </div>
+
+          </div>
+          
+          <div class="col-12">
+               <button type="submit" class="btn btn-primary">Cadastrar</button>
+          </div>
+
 
      </form>
 
