3bff70cab65e9a6e6122aef39f81e4d273e41af7
davidsstefano <davidsstefano@gmail.com>
2023-01-12 12:03:44 -0300

    master

--- class/data_hora.php
+++ class/data_hora.php
@@ -5,9 +5,7 @@
 $dia = date('d/m/Y');
 $hora = date('H:i' , strtotime('-4 hours'));
 
-echo $dia . "\n";
 
-echo $hora;
 
 
 ?>
--- public/pag_funcionario.php
+++ public/pag_funcionario.php
@@ -1,18 +1,29 @@
 <?php
 
 require_once '../class/retorno.php';
+require_once '../class/data_hora.php';
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>ADM</title>
 </head>
+
 <body>
-     <h1>Bem vindo a pagina de funcionario!!!</h1>
+     <h1><?php 
+          
+          echo $dia;
+          echo "<br>";
+          echo $hora;
+          ?></h1>
+
+
 </body>
+
 </html>
