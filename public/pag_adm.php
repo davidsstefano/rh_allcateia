9657033e00d15ce0ec933900ac379f5bc1731e25
davidsstefano <davidsstefano@gmail.com>
2023-01-12 16:36:46 -0300

    prodution

--- public/pag_adm.php
+++ public/pag_adm.php
@@ -6,15 +6,24 @@ require_once '../class/retorno.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+     <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+     <script src="https://kit.fontawesome.com/5f18a24258.js" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
      <title>ADM</title>
 </head>
+
 <body>
+     <a href="../api/logout.php" class="badge badge-danger">Sair</a>
      <a href="cadastro_funcionario.php">Cadastro de funcionario</a>
      <a href="cadastro_cargo.php">Cadastro cargo</a>
 
 </body>
+
 </html>
--- public/pag_funcionario.php
+++ public/pag_funcionario.php
@@ -14,12 +14,19 @@ require_once '../class/data_hora.php';
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+     <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+     <script src="https://kit.fontawesome.com/5f18a24258.js" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
      <title>ADM</title>
 </head>
 
 <body>
+     <button type="button" class="btn btn-danger"><a href="../api/logout.php" class="link-light">Sair</a></button>
      <h1>Ola <?php foreach ($db_usuario as $output) {
-                    echo   $output['nome'] ;echo " ";
+                    echo   $output['nome'];
+                    echo " ";
                     echo $output['sobrenome'];
                }; ?> </h1>
 
